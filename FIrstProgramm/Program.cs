b6bcaca8c1146c5109151969a881faf7b108c2aa
Andrey Berezin <ay.berez@gmail.com>
2020-05-26 22:14:38 +0300

    My first program

---
+++ FIrstProgramm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FIrstProgramm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
