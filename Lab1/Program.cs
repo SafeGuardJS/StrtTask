6149d35defe73beed0c4fec06d196a6c948d392a
Andrey Berezin <ay.berez@gmail.com>
2020-06-07 05:32:59 +0300

    Lab1 done

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,8 +6,16 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            float alpha, beta, z1, z2;
+            double alpha, beta, z1, z2;
+            Console.WriteLine("Enter alpha and beta");
+            alpha = Convert.ToDouble(Console.ReadLine()) * Math.PI / 180;
+            beta = Convert.ToDouble(Console.ReadLine()) * Math.PI / 180;
+            Console.WriteLine($"sin({alpha}) = {Math.Sin(alpha)}, Cos({alpha}) = {Math.Cos(alpha)}\n" + $"sin({beta}) = {Math.Sin(beta)}, Cos({beta}) = {Math.Cos(beta)}\n");
 
+            z1 = Math.Pow(Math.Cos(alpha) - Math.Cos(beta), 2) - Math.Pow(Math.Sin(alpha) - Math.Sin(beta), 2);
+            z2 = -4 * Math.Pow(Math.Sin((alpha - beta) / 2), 2) * Math.Cos(alpha + beta);
+
+            Console.WriteLine($"z1 = {z1}, z2 = {z2}");
         }
     }
 }
